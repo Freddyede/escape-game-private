f25381b0e12865d118e9a1a908eb1b5e096e4572
Franck <136079691+Freddyede@users.noreply.github.com>
2024-03-21 16:50:36 +0100

    Finish room 3

--- assets/js/rooms/room3.js
+++ assets/js/rooms/room3.js
@@ -1,27 +1,9 @@
-import * as Room3 from "./modules/Room3Modules.js";
-import {CanvasModules} from "./modules/Room3Modules.js";
+import { GameEngine } from "../../../src/Rooms/3/GameEngine.js";
 
 
+const game = new GameEngine()
 
-const gameEngine = new  Room3.GameEngine();
-
-gameEngine.getDomController().setElement('roomNumber3-gameRunner');
-const button = gameEngine.getDomController().getElement();
-gameEngine.getDomController().setDocumentListener(button, Room3.EventController.listenerElement.click, () => Room3.buttonHideMenu(button, document.getElementById('menu_container')));
-const canvas = document.querySelector('#containerCanvas');
-gameEngine.getCanvasController().getImagesController().setImage('assets/img/pngegg.png', 120, 78);
-const images = gameEngine.getCanvasController().getImagesController().getImage();
-CanvasModules.init({
-    element: canvas,
-    source: 'assets/img/pngegg.png',
-    pos: {
-        x: 120,
-        y: 78
-    }
-}, () => {
-    document.addEventListener('keydown', e => {
-        if(e.code === 'ArrowRight') {
-            console.log(images.x);
-        }
-    })
-});
+document.getElementById('startBtn').onclick = () => {
+    document.getElementById('menu').style = 'display: none'
+    game.run()
+}
