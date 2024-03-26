d326f3cc7e9089ed75803b9d854c2698d5094235
Franck <136079691+Freddyede@users.noreply.github.com>
2024-03-26 15:04:36 +0100

    Finish last update of room1 - Finish séquence Ahmed game

--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -1,7 +1,6 @@
 export const chargeArrayKey = (elmnt) => {
     let arrayLabels = document.querySelectorAll('.inventory-item');
     for (const elmnts of arrayLabels) {
-        console.log(elmnts.getAttribute('data-key'));
         if(elmnt.getAttribute('data-key').split('key')[1] === elmnts.getAttribute('data-target')) {
             elmnts.classList.remove('hide');
         }
